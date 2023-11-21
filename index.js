509a9bf722086ed4c2a65d1168c29efc8ae22174
Stephen <sagelvl40@gmail.com>
2023-11-21 22:39:16 +0000

    make the player

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ canvas.height = window.innerHeight;
 
 context.fillStyle = "black";
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+  draw() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.fillStyle = this.color;
+    context.fill();
+  }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, "white");
+player.draw();
