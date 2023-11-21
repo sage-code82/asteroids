e85839c2041ee04930a9fd45df6f31bb65774b3c
Stephen <sagelvl40@gmail.com>
2023-11-21 23:58:09 +0000

    add the laser dots, maths is hard

--- index.js
+++ index.js
@@ -24,8 +24,56 @@ class Player {
   }
 }
 
+class Laser {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.fillStyle = this.color;
+    context.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, "white");
-player.draw();
+const player = new Player(x, y, 30, "black");
+
+const lasers = [];
+
+function animate() {
+  requestAnimationFrame(animate);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  lasers.forEach((laser) => {
+    laser.update();
+  });
+}
+
+addEventListener("click", (event) => {
+  const angle = Math.atan2(
+    event.clientY - canvas.height / 2,
+    event.clientX - canvas.width / 2
+  );
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+  lasers.push(
+    new Laser(canvas.width / 2, canvas.height / 2, 5, "hotPink", velocity)
+  );
+});
+
+animate();
