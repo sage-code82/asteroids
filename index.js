4ad5267ecbbff4ba24ef696843fe76a502c98034
Stephen <sagelvl40@gmail.com>
2023-11-23 14:21:40 +0000

    spawn enemies and make them random sizes

--- index.js
+++ index.js
@@ -45,13 +45,56 @@ class Laser {
     this.y = this.y + this.velocity.y;
   }
 }
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.fillStyle = this.color;
+    context.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, "black");
-
 const lasers = [];
+const enemies = [];
+
+function spawn() {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4;
+    let x;
+    let y;
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+    const color = "green";
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -59,6 +102,9 @@ function animate() {
   player.draw();
   lasers.forEach((laser) => {
     laser.update();
+  });
+  enemies.forEach((enemy) => {
+    enemy.update();
   });
 }
 
@@ -77,3 +123,4 @@ addEventListener("click", (event) => {
 });
 
 animate();
+spawn();
