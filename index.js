359d9e2041eb022d9e446ac66aac6305f1316a89
Stephen <sagelvl40@gmail.com>
2023-11-25 16:32:05 +0000

    add explosions

--- index.js
+++ index.js
@@ -67,12 +67,43 @@ class Enemy {
   }
 }
 
+const friction = 0.99;
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.alpha = 1;
+  }
+  draw() {
+    context.save();
+    context.globalAlpha = this.alpha;
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.fillStyle = this.color;
+    context.fill();
+    context.restore();
+  }
+
+  update() {
+    this.draw();
+    this.velocity.x *= friction;
+    this.velocity.x *= friction;
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+    this.alpha -= 0.01;
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 15, "white");
 const lasers = [];
 const enemies = [];
+const particles = [];
 
 function spawn() {
   setInterval(() => {
@@ -102,6 +133,13 @@ function animate() {
   context.fillStyle = "rgba(0, 0, 0, 0.1)";
   context.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1);
+    } else {
+      particle.update();
+    }
+  });
   lasers.forEach((laser, index) => {
     laser.update();
 
@@ -128,10 +166,27 @@ function animate() {
       const distance = Math.hypot(laser.x - enemy.x, laser.y - enemy.y);
 
       if (distance - enemy.radius - laser.radius < 1) {
-        setTimeout(() => {
-          enemies.splice(index, 1);
-          lasers.splice(laserIndex, 1);
-        }, 0);
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(
+            new Particle(laser.x, laser.y, Math.random() * 2, enemy.color, {
+              x: (Math.random() - 0.5) * (Math.random() * 6),
+              y: (Math.random() - 0.5) * (Math.random() * 6),
+            })
+          );
+        }
+        if (enemy.radius - 10 > 5) {
+          gsap.to(enemy, {
+            radius: enemy.radius - 10,
+          });
+          setTimeout(() => {
+            lasers.splice(laserIndex, 1);
+          }, 0);
+        } else {
+          setTimeout(() => {
+            enemies.splice(index, 1);
+            lasers.splice(laserIndex, 1);
+          }, 0);
+        }
       }
     });
   });
